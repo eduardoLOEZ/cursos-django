82e59f85ca3439117f44cb5ba4601ba34533395b
Eduardo Loeza <eduardoloeza@Eduardos-MacBook-Air.local>
2024-11-30 12:39:54 -0600

    render.yml updated 4

--- project_name/settings.py
+++ project_name/settings.py
@@ -34,9 +34,11 @@ ALLOWED_HOSTS = []
 STRIPE_SECRET_KEY = config("STRIPE_SECRET_KEY")
 
 
+
 # Application definition
 
 INSTALLED_APPS = [
+    'whitenoise.runserver_nostatic',  # Asegura que whitenoise maneje los archivos estáticos
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -184,11 +186,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # This production code might break development mode, so we check whether we're in DEBUG mode
 if not DEBUG:
     # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
     # and renames the files with unique names for each version to support long-term caching
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
